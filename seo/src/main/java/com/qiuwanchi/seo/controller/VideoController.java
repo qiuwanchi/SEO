616f410d902e912b708459440e34d3a1309508b0
邱万迟 <ex_qiuwc1@partner.midea.com>
2022-06-08 10:39:52 +0800

    视频播放

--- seo/src/main/java/com/qiuwanchi/seo/controller/VideoController.java
+++ seo/src/main/java/com/qiuwanchi/seo/controller/VideoController.java
@@ -4,6 +4,7 @@ import com.qiuwanchi.seo.entity.Attachment;
 import com.qiuwanchi.seo.service.IAttachmentService;
 import com.qiuwanchi.seo.utils.FileConfiguration;
 import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
-@Log4j2
+@Slf4j
 @Controller
 public class VideoController {
 
@@ -42,20 +43,22 @@ public class VideoController {
         String rangeString = request.getHeader("Range");
 
         //打开本地文件流
-        String filePath = fileConfiguration.getResourceDir() + File.separator + "video" + File.pathSeparator + attachment.getFilepath().substring(9);
+        String filePath = fileConfiguration.getResourceDir() + File.separator + "video" + File.separator + attachment.getFilepath().substring(9);
+        log.info("播放的文件路径: {}", filePath);
         try {
             //获取响应的输出流
             OutputStream outputStream = response.getOutputStream();
             File file = new File(filePath);
             if(file.exists()){
+                log.info("文件存在：{}", filePath);
                 RandomAccessFile targetFile = new RandomAccessFile(file, "r");
                 long fileLength = targetFile.length();
                 //播放
                 if(rangeString != null){
-
+                    log.info("播放文件存：{}", filePath);
                     long range = Long.valueOf(rangeString.substring(rangeString.indexOf("=") + 1, rangeString.indexOf("-")));
                     //设置内容类型
-                    response.setHeader("Content-Type", attachment.getFiletype());
+                    response.setHeader("Content-Type", "video/mp4");
                     //设置此次相应返回的数据长度
                     response.setHeader("Content-Length", String.valueOf(fileLength - range));
                     //设置此次相应返回的数据范围
@@ -65,7 +68,7 @@ public class VideoController {
                     //设定文件读取开始位置（以字节为单位）
                     targetFile.seek(range);
                 }else {//下载
-
+                    log.info("下载文件存：{}", filePath);
                     //设置响应头，把文件名字设置好
                     response.setHeader("Content-Disposition", "attachment; filename="+attachment.getFilename() );
                     //设置文件长度
@@ -74,13 +77,13 @@ public class VideoController {
                     response.setHeader("Content-Type","application/octet-stream");
                 }
 
-
                 byte[] cache = new byte[1024 * 300];
                 int flag;
                 while ((flag = targetFile.read(cache))!=-1){
                     outputStream.write(cache, 0, flag);
                 }
             }else {
+                log.info("文件不存在：{}", filePath);
                 String message = "file:"+attachment.getFilename()+" not exists";
                 //解决编码问题
                 response.setHeader("Content-Type","application/json");
